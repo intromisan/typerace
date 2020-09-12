6189346275e74fe20d28f63ba336f92ecf3889d4
Serge Najimov <najimovserge@gmail.com>
2020-09-12 17:27:34 +0200

    done without css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const SNIPPETS = [
+    'Bears, beets, battlestar galactica',
+    "What's Forrest Gump's password? 1Forrest1",
+    'Where do programmers like to hangout? The Foo Bar'
+  ];
+  const INITIAL_GAME_STATE = { victory: false, startTime: null, endTime: null };
+  
+  const [userText, setUserText] = useState('');
+  const [snippet, setSnippet] = useState('');
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+
+  const updateUserText = event => {
+    setUserText(event.target.value);
+
+    if (event.target.value === snippet) {
+      setGameState({
+        ...gameState,
+        victory: true,
+        endTime: new Date().getTime() - gameState.startTime
+      });
+    }
+  }
+
+  const chooseSnippet = snippetIndex => () => {
+    console.log('setSnippet', snippetIndex);
+    setSnippet(SNIPPETS[snippetIndex]);
+    setGameState({ ...gameState, startTime: new Date().getTime() });
+  };
+
   return (
     <div className="App">
-      
+      <h2>Type Race</h2>
+      <hr />
+      <h3>Snippet</h3>
+      {snippet}
+      <h4>{gameState.victory ? `Done! 🎉 Time: ${gameState.endTime}ms` : null}</h4>
+      <input value={userText} onChange={updateUserText} />
+      <hr />
+      {
+        SNIPPETS.map((SNIPPET, index) => (
+          <button onClick={chooseSnippet(index)} key={index}>
+            {SNIPPET.substring(0, 10)}...
+          </button>
+        ))
+      }
     </div>
   );
 }
